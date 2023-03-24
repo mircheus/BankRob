e483c4a273190a17e2483b37d7f998d127cda04b
mircheus <mircheuss@yandex.ru>
2023-03-24 11:18:12 +0000

    grid script add

---
+++ Assets/Scripts/DragAndDrop/Grid.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Scripting;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private Slot[] _slots;
+
+    private void OnEnable()
+    {
+        foreach (var slot in _slots)
+        {
+            slot.RobbersCombined += OnRobbersCombined;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var slot in _slots)
+        {
+            slot.RobbersCombined -= OnRobbersCombined;
+        }
+    }
+
+    private void OnRobbersCombined()
+    {
+        foreach (var slot in _slots)
+        {
+            if (slot.IsFilled && slot.Robber.gameObject.activeSelf == false)
+            {
+                slot.Unfill();
+            }
+        }
+    }
+}
