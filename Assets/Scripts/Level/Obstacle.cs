eaac71690bd06612579de22661c5159d921c9a62
mircheus <mircheuss@yandex.ru>
2023-03-31 12:08:17 +0000

    add damage obstacle mechanic

--- Assets/Scripts/Level/Obstacle.cs
+++ Assets/Scripts/Level/Obstacle.cs
@@ -8,22 +8,37 @@ using UnityEngine.Events;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private int _health;
+    // [SerializeField] private GameObject _undamagedForm;
+    // [SerializeField] private GameObject _damagedForm;
+    [SerializeField] private GameObject[] _damagedForms;
+
+    private int _damageFormsAmount;
+    private int _currentDamage = 0;
     
     public event UnityAction Destroyed;
 
     private void OnEnable()
     {
-        Destroyed += DisableSelf;
+        Destroyed += OnDestroyed;
     }
 
     private void OnDisable()
     {
-        Destroyed -= DisableSelf;
+        Destroyed -= OnDestroyed;
+    }
+
+    private void Start()
+    {
+        _damageFormsAmount = _damagedForms.Length;
+        _currentDamage = 0;
+        InitializeUndamagedForm();
     }
 
     public void ApplyDamage(int damage)
     {
         _health -= damage;
+        // SwitchToDamagedForm();
+        NextDamageForm();
         
         if (_health <= 0)
         {
@@ -31,8 +46,38 @@ public class Obstacle : MonoBehaviour
         }
     }
 
-    private void DisableSelf()
+    private void OnDestroyed()
     {
         gameObject.SetActive(false);
+    }
+
+    private void InitializeUndamagedForm()
+    {
+        // _undamagedForm.SetActive(true);
+        _damagedForms[0].SetActive(true);
+        Debug.Log("point");
+
+        for (int i = 1; i < _damagedForms.Length; i++)
+        {
+            _damagedForms[i].SetActive(false);
+        }
+
+        // _damagedForm.SetActive(false); // для одной damage формы
+    }
+
+    // private void SwitchToDamagedForm()
+    // {
+    //     // _undamagedForm.SetActive(false);
+    //     _damagedForm.SetActive(true);
+    // }
+
+    private void NextDamageForm()
+    {
+        if (_currentDamage + 1 < _damageFormsAmount)
+        {
+            _damagedForms[_currentDamage].SetActive(false);
+            _currentDamage++;
+            _damagedForms[_currentDamage].SetActive(true);
+        }
     }
 }
