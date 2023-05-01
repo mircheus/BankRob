4da8cc47b46971643157f7bcd935ab1cb6af0932
mircheus <mircheuss@yandex.ru>
2023-05-01 10:00:17 +0200

    Adaptive Camera fix: works for any ratio, not for exact resolutions

--- Assets/Scripts/Camera/ScreenAdaptation.cs
+++ Assets/Scripts/Camera/ScreenAdaptation.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ScreenAdaptation : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _camera;
@@ -11,18 +12,45 @@ public class ScreenAdaptation : MonoBehaviour
     [SerializeField] private Vector3 _desktopDistance;
     [SerializeField] private int _mobileHeight;
     [SerializeField] private int _desktopHeight;
+    
 
     private void Start()
     {
-        if (Screen.height == _mobileHeight)
+        // SetCameraDistance();
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        Debug.Log(aspectRatio);
+        Debug.Log($"width = {Screen.width}");
+        Debug.Log($"height= {Screen.height}");
+        SetDistanceBy(aspectRatio);
+    }
+
+    private void Update()
+    {
+        //     SetCameraDistance();
+        // }
+    }
+
+    private void SetDistanceBy(float aspectRatio)
+    {
+        if (aspectRatio < 1)
         {
             transform.position = _mobileDistance;
-            Debug.Log(Screen.height);
+        }
+        else if (aspectRatio > 1)
+        {
+            transform.position = _desktopDistance;
+        }
+    }
+
+    private void SetCameraDistance()
+    {
+        if (Screen.height >= _mobileHeight)
+        {
+            transform.position = _mobileDistance;
         }
         else if (Screen.height == _desktopHeight)
         {
             transform.position = _desktopDistance;
-            Debug.Log(Screen.height);
         }
         
         Debug.Log(Screen.height);
