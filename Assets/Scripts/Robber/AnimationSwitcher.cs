d9b1494f890585af8b3f8c41d0e6e7c05cbe52ce
mircheus <mircheuss@yandex.ru>
2023-05-10 10:20:19 +0200

    add die state to AnimationSwitcher.cs

--- Assets/Scripts/Robber/AnimationSwitcher.cs
+++ Assets/Scripts/Robber/AnimationSwitcher.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 
 public class AnimationSwitcher : MonoBehaviour
 {
-   [SerializeField] private ObstacleCrusher obstacleCrusher;
+   [SerializeField] private ObstacleCrusher _obstacleCrusher;
+   [SerializeField] private RobberMovement _robberMovement;
    
    private Animator _animator;
    // private int _attack = Animator.StringToHash("Attack");
    private int _attack = Animator.StringToHash("axe_attack_overdrive");
    private int _attackingWall = Animator.StringToHash("AttackingWall");
    private int _firstAttack = Animator.StringToHash("first_attack");
+   private int _died = Animator.StringToHash("Died");
    
    private void OnEnable()
    {
       // _wallCrusher.WallCollided += PlayAttackAnimation;
-      obstacleCrusher.ObstacleCollided += SwitchToAttackState;
-      obstacleCrusher.ObstacleDestroyed += SwitchToFallState;
+      _obstacleCrusher.ObstacleCollided += SwitchToAttackState;
+      _obstacleCrusher.ObstacleDestroyed += SwitchToFallState;
+      _robberMovement.GetStopped += SwitchToDieState;
    }
 
    private void OnDisable()
    {
       // _wallCrusher.WallCollided -= PlayAttackAnimation;
-      obstacleCrusher.ObstacleCollided -= SwitchToAttackState;
-      obstacleCrusher.ObstacleDestroyed -= SwitchToFallState;
+      _obstacleCrusher.ObstacleCollided -= SwitchToAttackState;
+      _obstacleCrusher.ObstacleDestroyed -= SwitchToFallState;
+      _robberMovement.GetStopped -= SwitchToDieState;
    }
 
    private void Start()
@@ -47,5 +51,10 @@ public class AnimationSwitcher : MonoBehaviour
    private void SwitchToFallState()
    {
       _animator.SetBool(_attackingWall, false);
+   }
+
+   private void SwitchToDieState()
+   {
+      _animator.SetTrigger(_died);
    }
 }
