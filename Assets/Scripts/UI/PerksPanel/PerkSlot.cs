cdf5ca2d4540548017735ac84285073928eb1692
mircheus <mircheuss@yandex.ru>
2023-05-31 16:01:55 +0200

    Add block perk after GetStopped

--- Assets/Scripts/UI/PerksPanel/PerkSlot.cs
+++ Assets/Scripts/UI/PerksPanel/PerkSlot.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PerkSlot : MonoBehaviour
 {
-    [SerializeField] private FreezedButton _freezedButton;
+    [SerializeField] private PerkBlocker _freezedButton;
+    [SerializeField] private PerkBlocker _deadImage;
     
     private PerkActivator _perkActivator;
     private int _columnIndex = -1;
@@ -25,12 +27,24 @@ public class PerkSlot : MonoBehaviour
     public void SubscribeToRobber(Robber robber)
     {
         robber.Frozen += OnFrozen;
-        Debug.Log("Subscribed to OnFrozen");
+        robber.GetComponent<RobberMovement>().GetStopped += OnGetStopped;
     }
 
     private void OnFrozen(int columnIndex)
     {
-        _freezedButton.gameObject.transform.SetSiblingIndex(1);
+        // SetActiveToTop(_freezedButton);
+        _freezedButton.gameObject.transform.SetAsLastSibling();
         _freezedButton.gameObject.SetActive(true);
+    }
+
+    private void OnGetStopped()
+    {
+        SetActiveToTop(_deadImage);
+    }
+
+    private void SetActiveToTop(PerkBlocker perkBlocker)
+    {
+        perkBlocker.gameObject.transform.SetAsLastSibling();
+        perkBlocker.gameObject.SetActive(true);
     }
 }
