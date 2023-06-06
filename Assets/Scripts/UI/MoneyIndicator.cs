a5884445281a381b272b76c00aa457a2cfd27aca
mircheus <mircheuss@yandex.ru>
2023-06-06 14:57:06 +0200

    Add reward money indication

--- Assets/Scripts/UI/MoneyIndicator.cs
+++ Assets/Scripts/UI/MoneyIndicator.cs
@@ -5,34 +5,53 @@ using TMPro;
 using UnityEngine;
 
 public class MoneyIndicator : MonoBehaviour
-{
+{   
+    [Header("Prepare stage")]
     [SerializeField] private PlayerData _playerData;
     [SerializeField] private TMP_Text _moneyIndicator;
+
+    [Header("Win stage")] 
+    [SerializeField] private Robbery _robbery;
+    [SerializeField] private TMP_Text _rewardMoney;
+
+    private const string PlusSign = "+";
     
     private void OnEnable()
     {
         _playerData.DataLoaded += OnDataLoaded;
         _playerData.DataUpdated += OnDataUpdated;
+        _robbery.BankRobbed += OnBankRobbed;
     }
 
     private void OnDisable()
     {
         _playerData.DataLoaded -= OnDataLoaded;
         _playerData.DataUpdated -= OnDataUpdated;
+        _robbery.BankRobbed -= OnBankRobbed;
     }
 
     private void OnDataLoaded()
     {
-        SetMoneyAmount();
+        ShowMoneyAmount();
     }
 
     private void OnDataUpdated()
     {
-        SetMoneyAmount();
+        ShowMoneyAmount();
     }
 
-    private void SetMoneyAmount()
+    private void OnBankRobbed()
+    {
+        ShowRewardAmount();
+    }
+
+    private void ShowMoneyAmount()
     {
         _moneyIndicator.text = _playerData.MoneyAmount.ToString();
+    }
+
+    private void ShowRewardAmount()
+    {
+        _rewardMoney.text = PlusSign + _robbery.MoneyRewardAmount;
     }
 }
