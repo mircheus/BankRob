376fb5ef610ee6d594798caeb2da0e4ae7450a4f
mircheus <mircheuss@yandex.ru>
2023-06-29 15:01:20 +0200

    add allMoneyCounter loading from data

--- Assets/Scripts/PlayerStats/PlayerData.cs
+++ Assets/Scripts/PlayerStats/PlayerData.cs
@@ -152,7 +152,7 @@ public class PlayerData : MonoBehaviour
         if (_dataManager.IsLoadDataPersists())
         {
             Data loadedData = _dataManager.LoadData();
-            SetPlayerStats(loadedData.Money, loadedData.Keys, loadedData.CompletedLevelsCounter, loadedData.PreviousLevelFloorsAmount, loadedData.AliveRobbers, loadedData.AchievedLevels, loadedData.CurrentPrice, loadedData.CurrentReward, loadedData.IsTryAgain, loadedData.IsAuthorized);
+            SetPlayerStats(loadedData.Money, loadedData.AllMoneyCounter, loadedData.CompletedLevelsCounter, loadedData.PreviousLevelFloorsAmount, loadedData.AliveRobbers, loadedData.AchievedLevels, loadedData.CurrentPrice, loadedData.CurrentReward, loadedData.IsTryAgain, loadedData.IsAuthorized);
         }
         else
         {
@@ -170,10 +170,10 @@ public class PlayerData : MonoBehaviour
         SavePlayerStats(_moneyAmount, _allMoneyCounter, _keysAmount, _completedLevelsCounter, barriersProgression.FloorsQuantity, _robbersToSave, _achievedLevels, _economicProgression.CurrentPrice, _economicProgression.RewardToNextLevel, _isTryAgain, _isAuthorized);
     }
 
-    private void SetPlayerStats(int moneyAmount, int keysAmount, int completedLevelsAmount, int floorsAmount, int[] aliveRobbers, int achievedLevels, int currentPrice, int currentReward, bool isTryAgain, bool isAuthorized)
+    private void SetPlayerStats(int moneyAmount, int allMoneyCounter, int completedLevelsAmount, int floorsAmount, int[] aliveRobbers, int achievedLevels, int currentPrice, int currentReward, bool isTryAgain, bool isAuthorized)
     {
         _moneyAmount = moneyAmount;
-        _keysAmount = keysAmount;
+        _allMoneyCounter = allMoneyCounter;
         _completedLevelsCounter = completedLevelsAmount;
         _floorsAmountFromPreviousLevel = floorsAmount;
         _robbersToSave = aliveRobbers;
