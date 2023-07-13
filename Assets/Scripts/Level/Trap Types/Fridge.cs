033caa8ed8028d85afcfbf837ef9424ab2297ce2
mircheus <mircheuss@yandex.ru>
2023-07-13 23:25:39 +0200

    refactor: deleted comment lines

--- Assets/Scripts/Level/Trap Types/Fridge.cs
+++ Assets/Scripts/Level/Trap Types/Fridge.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(AudioOneShot))]
 public class Fridge : Trap
