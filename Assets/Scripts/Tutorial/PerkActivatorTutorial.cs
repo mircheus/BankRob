a5e78d728d5c61db0c2adad843e51ec11a72c3df
mircheus <mircheuss@yandex.ru>
2023-07-14 01:55:31 +0200

    delete commented lines

--- Assets/Scripts/Tutorial/PerkActivatorTutorial.cs
+++ Assets/Scripts/Tutorial/PerkActivatorTutorial.cs
@@ -7,27 +7,12 @@ using UnityEngine.UI;
 
 public class PerkActivatorTutorial : MonoBehaviour
 {
-    // [SerializeField] private GameObject _perk;
     [SerializeField] private Button _button;
     [SerializeField] private float _coolDownTime;
     [SerializeField] private PerksPanel _perksPanel;
 
     private Robber _robber;
     private int _columnIndex;
-
-    // public event UnityAction<int> PerkActivated;
-
-    // public int ColumnIndex => _columnIndex;
-    //
-    // public void SetColumnIndex(int index)
-    // {
-    //     _columnIndex = index;
-    // }
-
-    // public void SetRobber(Robber robber)
-    // {
-    //     _robber = robber;
-    // }
 
     private void OnEnable()
     {
