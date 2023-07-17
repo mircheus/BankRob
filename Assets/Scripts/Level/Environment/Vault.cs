e04e01f6a12b09b49f072636910552f5efab4764
mircheus <mircheuss@yandex.ru>
2023-07-17 10:57:32 +0200

    remove GetComponent

--- Assets/Scripts/Level/Environment/Vault.cs
+++ Assets/Scripts/Level/Environment/Vault.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class Vault : MonoBehaviour
 {
     private Animator _animator;
+    private AudioOneShot _audioOneShot;
     private int _openVault = Animator.StringToHash("OpenVault");
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _audioOneShot = GetComponent<AudioOneShot>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,7 +18,7 @@ public class Vault : MonoBehaviour
         if (other.TryGetComponent(out Robber robber))
         {
             _animator.Play(_openVault);
-            GetComponent<AudioOneShot>().PlayOneShot();
+            _audioOneShot.PlayOneShot();
             robber.ReachedVault?.Invoke(robber);
         }
     }
